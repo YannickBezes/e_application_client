e9584ca654b84e2aa58b18bff7df94e926d817a2
YannickBezes <yannick.bezes@gmail.com>
2019-12-31 13:05:35 +0100

    package.json

---
+++ build/precache-manifest.a0faf03c11bc4463c7966eafa3e6800b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8fd40eb60f9af83743712b17f92dc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48dbe08f5452f5dcfbb9",
+    "url": "/static/css/main.7d386ac6.chunk.css"
+  },
+  {
+    "revision": "0e6e932fe650e7ea3c58",
+    "url": "/static/js/2.f7440803.chunk.js"
+  },
+  {
+    "revision": "48dbe08f5452f5dcfbb9",
+    "url": "/static/js/main.480f11e5.chunk.js"
+  },
+  {
+    "revision": "cee1cfaa79530a73c915",
+    "url": "/static/js/runtime-main.2d040558.js"
+  }
+]);
